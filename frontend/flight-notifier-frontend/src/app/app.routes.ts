0cb3d8de16f37efa40bc088a44506f3472cf7de3
TragicVibesHd <shivan562@yahoo.com>
2026-07-06 16:38:40 -0400

    Add portal route protected by auth guard

--- frontend/flight-notifier-frontend/src/app/app.routes.ts
+++ frontend/flight-notifier-frontend/src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { Login } from './components/login/login';
+import { Portal } from './components/portal/portal';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
   { path: 'login', component: Login },
+  { path: 'portal', component: Portal, canActivate: [authGuard] },
   { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
