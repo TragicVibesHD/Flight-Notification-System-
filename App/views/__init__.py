70e0e0c449c51c6e1f28245f51c0af09eaeec6a6
TragicVibesHd <shivan562@yahoo.com>
2026-07-06 14:58:02 -0400

    Register customer and notification views

--- App/views/__init__.py
+++ App/views/__init__.py
@@ -2,7 +2,9 @@
 from .user import user_views
 from .index import index_views
 from .auth import auth_views
+from .customer import customer_views
+from .notification import notification_views
 
 
-views = [user_views, index_views, auth_views]
+views = [user_views, index_views, auth_views, customer_views, notification_views]
 # blueprints must be added to this list
