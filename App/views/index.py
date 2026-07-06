22b15501931016f403fac6d5cf3c59dec9083ef1
TragicVibesHd <shivan562@yahoo.com>
2026-07-06 11:12:45 -0400

    Strip page-rendering route from index views, keep API routes only

--- App/views/index.py
+++ App/views/index.py
@@ -1,11 +1,7 @@
-from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify
-from App.controllers import create_user, initialize
+from flask import Blueprint, jsonify
+from App.controllers import initialize
 
-index_views = Blueprint('index_views', __name__, template_folder='../templates')
-
-@index_views.route('/', methods=['GET'])
-def index_page():
-    return render_template('index.html')
+index_views = Blueprint('index_views', __name__)
 
 @index_views.route('/init', methods=['GET'])
 def init():
@@ -14,4 +10,4 @@ def init():
 
 @index_views.route('/health', methods=['GET'])
 def health_check():
-    return jsonify({'status':'healthy'})
+    return jsonify({'status': 'healthy'})
