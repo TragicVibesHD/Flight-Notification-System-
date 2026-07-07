d7d10bc00b4f05274fa102e21410d95ad681f15a
TragicVibesHd <shivan562@yahoo.com>
2026-07-07 08:52:47 -0400

    Register flight views

--- App/views/__init__.py
+++ App/views/__init__.py
@@ -1,10 +1,12 @@
+# blue prints are imported 
 # explicitly instead of using *
 from .user import user_views
 from .index import index_views
 from .auth import auth_views
 from .customer import customer_views
+from .flight import flight_views
 from .notification import notification_views
 
 
-views = [user_views, index_views, auth_views, customer_views, notification_views]
+views = [user_views, index_views, auth_views, customer_views, flight_views, notification_views]
 # blueprints must be added to this list
