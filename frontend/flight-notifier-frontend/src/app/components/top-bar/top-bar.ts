4ebafe7a976b96a06ead170145cca5a14fc18131
TragicVibesHd <shivan562@yahoo.com>
2026-07-07 09:49:02 -0400

    Build top-bar component with logo and admin greeting

--- frontend/flight-notifier-frontend/src/app/components/top-bar/top-bar.ts
+++ frontend/flight-notifier-frontend/src/app/components/top-bar/top-bar.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-top-bar',
   imports: [],
   templateUrl: './top-bar.html',
-  styleUrl: './top-bar.css',
+  styleUrl: './top-bar.css'
 })
-export class TopBar {}
+export class TopBar {
+  auth = inject(Auth);
+}
