70a77a80433bb1589f3d303a4a12a76fd33df553
TragicVibesHd <shivan562@yahoo.com>
2026-07-07 14:44:08 -0400

    Remove gevent worker type from gunicorn config, use default sync workers

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -2,14 +2,10 @@
 import multiprocessing
 
 # The socket to bind.
-# "0.0.0.0" to bind to all interfaces. 8000 is the port number.
 bind = "0.0.0.0:8080"
 
 # The number of worker processes for handling requests.
 workers = 4
-
-# Use the 'gevent' worker type for async performance.
-worker_class = 'gevent'
 
 # Log level
 loglevel = 'info'
